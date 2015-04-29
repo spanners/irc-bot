0b08c19eee08386f8554d78a61ef76071d10ba02
Simon Buist <simon.buist@gmail.com>
2015-04-29 02:47:16 +0100

    Wait before 3000 ms before joining channel

--- bot.hs
+++ bot.hs
@@ -8,23 +8,12 @@ import System.IO
 import Data.List
 import Data.Char
 import System.Random
+import Control.Concurrent
 
 server = "irc.freenode.org"
 port = 6667
 channel = "##bots"
-nickname = "IRCBot"
-
-main = do
-  h <- connectTo server (PortNumber (fromIntegral port))
-  hSetBuffering   h NoBuffering
-  hSetNewlineMode h (NewlineMode CRLF CRLF)
-
-  nick h nickname
-  user h nickname
-
-  joinChan h channel
-
-  forever (action h)
+nickname = "JustAnotherIRCBot"
 
 action :: Handle -> IO ()
 action h = do
@@ -32,7 +21,7 @@ action h = do
   putStrLn line
   case line of
     PING serv           -> pong h serv
-    JOIN Bot chan       -> msg h chan "Hello, I am " ++ nickname
+    JOIN Bot chan       -> msg h chan $ "Hello, I am" ++ nickname
     JOIN nick chan      -> msg h chan ("Hi, " ++ nick ++ ". Welcome to " ++ chan)
     PM   from DoJoin    -> joinChan h channel
     PM   from m         -> msg h from ("You said \"" ++ m ++ "\" to me?!")
@@ -41,6 +30,20 @@ action h = do
       roll :: Int <- randomRIO (1, 6)
       msg h chan (from ++ ": You rolled " ++ show roll)
     _                   -> return ()
+
+pattern Bot = "JustAnotherIRCBot"
+pattern JOIN nick chan 
+   <- (words -> [getNick -> Just nick, "JOIN", chan])
+pattern PING serv <- (words -> ["PING", serv])
+pattern Nick n <- ((\a -> (head a /= '#', a)) -> (True, n))
+pattern Chan c <- ((\a -> (head a == '#', a)) -> (True, c))
+pattern PM from m <- (getPriv -> Just (from, Nick Bot,  m))
+pattern MSG from to m <- (getPriv -> Just (from, Chan to, m))
+pattern Cat <- (isInfixOf "cat" . map toLower -> True)
+pattern Command cmd = '>':' ':cmd
+pattern Roll <- Command (map toLower -> "roll")
+pattern DoJoin <- Command (map toLower -> "do_join")
+
 
 -- Choose a nick
 nick :: Handle -> String -> IO ()
@@ -56,7 +59,7 @@ joinChan h chan = do
     putStrLn ("JOIN" ++ chan)
     hPutStrLn h ("JOIN " ++ chan)
 
--- Respond to ping with "PONG"
+-- Respond to "PING" with "PONG"
 pong :: Handle -> String -> IO ()
 pong h serv = hPutStrLn h ("PONG " ++ serv)
 
@@ -67,6 +70,7 @@ getNick (':':prefix) = do
   return (take index prefix)
 getNick _            = Nothing
 
+-- Send message to channel
 msg :: Handle -> String -> String -> IO ()
 msg h chan msg = hPutStrLn h ("PRIVMSG " ++ chan ++ " :" ++ msg)
 
@@ -80,15 +84,16 @@ getPriv msg = case words msg of
   where
   clean = tail . dropWhile (/=':') . dropWhile (/= ' ') . tail
 
-pattern Bot = nickname
-pattern JOIN nick chan 
-   <- (words -> [getNick -> Just nick, "JOIN", chan])
-pattern PING serv <- (words -> ["PING", serv])
-pattern Nick n <- ((\a -> (head a /= '#', a)) -> (True, n))
-pattern Chan c <- ((\a -> (head a == '#', a)) -> (True, c))
-pattern PM from m <- (getPriv -> Just (from, Nick Bot,  m))
-pattern MSG from to m <- (getPriv -> Just (from, Chan to, m))
-pattern Cat <- (isInfixOf "cat" . map toLower -> True)
-pattern Command cmd = '>':' ':cmd
-pattern Roll <- Command (map toLower -> "roll")
-pattern DoJoin <- Command (map toLower -> "do_join")
+main = do
+  h <- connectTo server (PortNumber (fromIntegral port))
+  hSetBuffering   h NoBuffering
+  hSetNewlineMode h (NewlineMode CRLF CRLF)
+
+  nick h nickname
+  user h nickname
+
+  threadDelay 3000
+
+  joinChan h channel
+
+  forever (action h)
